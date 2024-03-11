aa837f818cc85a566183adf284f71f3a6463e56e
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2024-03-11 20:05:55 +0000

    include society data

--- src/utils.py
+++ src/utils.py
@@ -44,6 +44,16 @@ def get_generated_date(data):
 def convert_generated_dt_to_object(generated_dt):
     return datetime.strptime(generated_dt, "%Y-%m-%dT%H%M%S")
 
+def combine_json_data(general_data, soc_data):
+    # because MSL is fucking STUPID
+    # we have to merge the two json objects
+    # both objects have the same initial data, followed by "Groups"
+    # put all the items from "Groups" from soc_data into general_data and return
+    
+    general_data['Groups'] = general_data['Groups'] + soc_data['Groups']
+
+    return general_data
+
 
 
 def get_all_election_data():
@@ -62,10 +72,14 @@ def get_all_election_data():
     10 - College
     """
 
-    election_url = "https://www.guildofstudents.com/svc/voting/stats/election/paramstats/189?groupIds=1,6,7,8,9,10&sortBy=itemname&sortDirection=ascending"
+    general_data_url = "https://www.guildofstudents.com/svc/voting/stats/election/paramstats/189?groupIds=1,6,7,8,9,10&sortBy=itemname&sortDirection=ascending"
+    soc_data_url = "https://www.guildofstudents.com/svc/voting/stats/election/membershipstats/189?groupIds=1,2,3,4,5,6,7,8,9,10&sortBy=itemname&sortDirection=ascending"
 
-    data = get_data(election_url)
+    general_data = get_data(general_data_url)
+    soc_data = get_data(soc_data_url)
 
-    date_generated = get_generated_date(data)
+    date_generated = get_generated_date(general_data)
 
-    save_json_data(data, "../data/json/raw/" + date_generated + ".json")
+    all_data = combine_json_data(general_data, soc_data)
+
+    save_json_data(all_data, "../data/json/raw/" + date_generated + ".json")
